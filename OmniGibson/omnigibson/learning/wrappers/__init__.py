398ff024db4c5b5e8be0fd38e632bc00579eb470
ljc-1222 <20031222tony@gmail.com>
2026-06-06 01:26:14 +0000

    Add openpi behavior learning patch

--- OmniGibson/omnigibson/learning/wrappers/__init__.py
+++ OmniGibson/omnigibson/learning/wrappers/__init__.py
@@ -1,6 +1,13 @@
 from .default_wrapper import DefaultWrapper
 from .heavy_robot_wrapper import HeavyRobotWrapper
 from .rgb_low_res_wrapper import RGBLowResWrapper
+from .rgb_wrapper import RGBWrapper
 from .rich_obs_wrapper import RichObservationWrapper
 
-__all__ = ["DefaultWrapper", "HeavyRobotWrapper", "RGBLowResWrapper", "RichObservationWrapper"]
+__all__ = [
+    "DefaultWrapper",
+    "HeavyRobotWrapper",
+    "RGBLowResWrapper",
+    "RGBWrapper",
+    "RichObservationWrapper",
+]
